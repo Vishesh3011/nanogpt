63f58123833eb9a79ec99fbd32f09a5f12672998
Vishesh Tarun Modi <u8186280@anu.edu.au>
2026-03-17 19:00:57 +1100

    added required files for the python scripts to run on google colab

---
+++ config/train_rocstories.py
@@ -0,0 +1,35 @@
+dataset = 'rocstories'
+device = 'cpu' # or 'cpu'
+compile = False
+
+out_dir = 'out-rocstories'
+eval_interval = 500
+eval_iters = 200
+log_interval = 10
+
+always_save_checkpoint = True
+
+wandb_log = False
+wandb_project = 'rocstories'
+wandb_run_name = 'nanogpt'
+
+dataset = 'rocstories'
+
+gradient_accumulation_steps = 2
+batch_size = 16
+block_size = 128  # important for stories
+
+# model size (balanced for performance vs speed)
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.1   # less than shakespeare
+
+learning_rate = 3e-4   # VERY IMPORTANT (lower than shakespeare)
+max_iters = 6000
+lr_decay_iters = 6000
+min_lr = 3e-5
+
+beta2 = 0.99
+
+warmup_iters = 200
