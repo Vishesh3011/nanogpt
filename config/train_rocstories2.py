c8d20c8c78382cba0bd14e1610b3d2bf9c6ed8f8
Vishesh Tarun Modi <u8186280@anu.edu.au>
2026-03-19 20:47:55 +1100

    added eot for better ppl

---
+++ config/train_rocstories2.py
@@ -0,0 +1,35 @@
+dataset = 'rocstories'
+device = 'cuda' # or 'cpu'
+compile = False
+
+out_dir = 'out-rocstories'
+eval_interval = 500
+eval_iters = 200
+log_interval = 10
+
+always_save_checkpoint = True
+
+wandb_log = False
+wandb_project = 'rocstories'
+wandb_run_name = 'nanogpt'
+
+dataset = 'rocstories'
+
+batch_size = 16
+gradient_accumulation_steps = 4
+block_size = 256  # important for stories
+
+# model size (balanced for performance vs speed)
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.05   # less than shakespeare
+
+learning_rate = 3e-4   # VERY IMPORTANT (lower than shakespeare)
+max_iters = 6000
+lr_decay_iters = 6000
+min_lr = 3e-5
+
+beta2 = 0.99
+
+warmup_iters = 200
---
+++ data/rocstories/prepare3.py
@@ -0,0 +1,51 @@
+import tiktoken
+import numpy as np
+
+from datasets import load_dataset
+from datasets import Dataset
+
+def load_data_from_repo() -> tuple[Dataset, Dataset]:
+    dataset = load_dataset("mintujupally/ROCStories")
+    train_data = []
+    test_data = []
+    train_data = dataset['train']
+    test_data = dataset['test']
+    return (train_data, test_data)
+
+def get_story_from_row(row) -> str:
+    return row["text"]
+
+def get_stories(data) -> tuple[list[str]]:
+    stories = []
+    for row in data:
+        story = get_story_from_row(row)
+        stories.append(story)
+    return stories
+
+def get_text(stories) -> str:
+    return "<|endoftext|>".join(stories) + "<|endoftext|>"
+
+def get_encodings(text: str) -> list[int]:
+    enc = tiktoken.get_encoding("gpt2")
+    return enc.encode(text, allowed_special={"<|endoftext|>"})
+
+if __name__ == "__main__":
+    train_data, test_data = load_data_from_repo()
+
+    train_stories = get_stories(train_data)
+    test_stories = get_stories(test_data)
+
+    train_text = get_text(train_stories)
+    test_text = get_text(test_stories)
+
+    train_encodings = get_encodings(train_text)
+    test_encodings = get_encodings(test_text)
+
+    train_ids = np.array(train_encodings, dtype=np.uint16)
+    val_ids = np.array(test_encodings, dtype=np.uint16)
+
+    print(f"train has {len(train_ids):,} tokens")
+    print(f"val has {len(val_ids):,} tokens")
+
+    train_ids.tofile('data/rocstories/train.bin')
+    val_ids.tofile('data/rocstories/val.bin')
