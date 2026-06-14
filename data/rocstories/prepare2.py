e42cba9b7af18d6dfd591ba2d6859d03a20f5bd2
Vishesh Tarun Modi <u8186280@anu.edu.au>
2026-06-14 23:55:59 +1000

    refactored the file structure

--- data/rocstories/prepare2.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("mintujupally/ROCStories")
-
-test_data = dataset['test']
-stories = [row["text"] for row in test_data]
-
-with open("data/rocstories/eval_stories.txt", "w") as f:
-    f.write("\n\n".join(stories))
--- init-task2.sh
+++
@@ -1,3 +0,0 @@
-python3 data/code_search_net/prepare.py
-
-python3 train_for_task2.py config/code-gen.py
